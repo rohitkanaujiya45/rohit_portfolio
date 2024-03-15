71f5e0d97cb2b7267d300dad65896f3a3874f281
Rohit Kanaujiya <127976490+rohitkanaujiya45@users.noreply.github.com>
2024-03-15 11:51:01 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,32 +9,59 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  const [heightOfMyScreen, setHeightOfMyScreen] = useState(window.innerHeight);
+  const [widthOfMyScreen, setWidthOfMyScreen] = useState(window.innerWidth);
 
   useEffect(() => {
-    setWidth(window.innerWidth);
+    // Update the screen height when the window is resized
+    const handleResize = () => {
+      setHeightOfMyScreen(window.innerHeight);
+      setWidthOfMyScreen(window.innerWidth);
+    };
+
+    // Add an event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const [isMobile, setIsMobile] = useState({});
+
+  useEffect(() => {
+    if (heightOfMyScreen > 621) {
+      setIsMobile({
+        scaleProp: 0.6,
+        heightProp: '31rem'
+      })
+    } else {
+      setIsMobile({
+        scaleProp: 1.6,
+        heightProp: '83rem'
+      })
+    }
   }, []);
 
   return (
     <div>
       <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
+        {/* <Particle /> */}
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          {/* {console.log("height: " + heightOfMyScreen)}
+          {heightOfMyScreen}
+          <br />
+          {console.log("width: " + widthOfMyScreen)}
+          {widthOfMyScreen} */}
+          {/* <div style={{ height: heightOfMyScreen > '606' ? '800px' : '400px' , overflowY: "hidden" }}> */}
+          {/* <div style={{ height: '33rem' , overflowY: "hidden" }}> */}
+          <div style={{ height: `${isMobile.heightProp}`, overflowY: "hidden" }}>
+            <Document file={pdf} className="d-flex justify-content-center">
+              {/* <Page pageNumber={1} scale={0.6} /> */}
+              <Page pageNumber={1} scale={isMobile.scaleProp} />
+            </Document>
+          </div>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
